10119f112a252e82e337a5969315828a4354bcb8
Vitor Schweder <developervphantom@gmail.com>
2020-02-02 21:49:31 -0300

    remove gitignore files

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    res.json({message: 'hello world'});
+  res.send('<h1>hello world</h1>');
 });
 
 export default routes;
